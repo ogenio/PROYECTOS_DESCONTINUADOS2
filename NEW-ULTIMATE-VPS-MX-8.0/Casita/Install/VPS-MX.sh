5e57efaa5f5e1a40aaa218a52816100ce782391f
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2021-01-18 02:38:39 -0300

    Update Script

--- NEW-ULTIMATE-VPS-MX-8.0/Casita/Install/VPS-MX.sh
+++ NEW-ULTIMATE-VPS-MX-8.0/Casita/Install/VPS-MX.sh
@@ -197,7 +197,7 @@ wget -O /bin/rebootnb https://raw.githubusercontent.com/VPS-MX/VPS-MX-8.0/master
 chmod +x /bin/rebootnb 
 wget -O /bin/resetsshdrop https://raw.githubusercontent.com/VPS-MX/VPS-MX-8.0/master/ArchivosUtilitarios/resetsshdrop &> /dev/null
 chmod +x /bin/resetsshdrop
-wget -O /etc/versin_script_new https://raw.githubusercontent.com/VPS-MX/VPS-MX-8.0/master/Vercion &>/dev/null
+wget -O /etc/versin_script_new https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/PROYECTOS_DESCONTINUADOS/master/NEW-ULTIMATE-VPS-MX-8.0/Casita/Install/vercion &>/dev/null
 msg -bar2
 echo -e "	\e[1;44mCAMBIAR  RESELLER\e[0m"
 msg -bar
