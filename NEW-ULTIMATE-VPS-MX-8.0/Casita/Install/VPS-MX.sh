d6970893c4bdf65b8dfdba93e6c8fb626ba86baf
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2021-03-17 11:24:04 -0300

    Update Script

--- NEW-ULTIMATE-VPS-MX-8.0/Casita/Install/VPS-MX.sh
+++ NEW-ULTIMATE-VPS-MX-8.0/Casita/Install/VPS-MX.sh
@@ -337,7 +337,7 @@ Key="LaCasitaDelTerrorK?%ab97cda8f?K81mx@+95+94@"
 REQUEST=$(echo $kalix1|$PUTO)
 echo "$IP" > /usr/bin/vendor_code
 cd $HOME
-msg -ne "Key: "
+msg -ne "Files: "
 wget -O $HOME/lista-arq ${REQUEST}/lista-arq > /dev/null 2>&1 && echo -e "\033[1;32m Verificado" || {
    echo -e "\033[1;32m Verificada"
    invalid_key
